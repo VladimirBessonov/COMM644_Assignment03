aadbb3285c6a04a2b9fb264b070b8bf7ef35f371
vladimirbessonov <2000.besson@gmail.com>
2020-02-28 00:11:37 -0800

    01-functions-practice

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,9 +1,10 @@
 //STEP 1 - v.1
-// function halfNumber() {
-//     let value = Number(window.prompt("Enter any number"));
-//     window.document.write("Half of " + value + " is " + value/2 + ".");
-// }
-// halfNumber();
+function halfNumber() {
+    let value = Number(window.prompt("Enter any number"));
+    window.document.write('STEP 1'+ '\n')
+    window.document.write("Half of " + value + " is " + value/2 + "." + '\n');
+}
+halfNumber();
 
 //STEP 1 - v.2
 // function halfNumber(value) {
@@ -11,12 +12,13 @@
 // }
 // halfNumber(7);
 
-//STEP 2 - v.1
-// function squarefNumber() {
-//     let value = Number(window.prompt("Enter any number"));
-//     window.document.write("The result of squaring the number " + value + " is " + Math.pow(value, 2) + ".");
-// }
-//  squarefNumber();
+// STEP 2 - v.1
+function squarefNumber() {
+    let value = Number(window.prompt("Enter any number"));
+    window.document.write('STEP 2'+ '\n')
+    window.document.write("The result of squaring the number " + value + " is " + Math.pow(value, 2) + "." + '\n');
+}
+ squarefNumber();
 
 //STEP 2 - v.2
 // function squarefNumber(value) {
@@ -25,12 +27,13 @@
 //  squarefNumber(7);
 
 //STEP 3 - v.1
-// function percentOf() {
-//     let num1 = Number(window.prompt("Enter the first number"));
-//     let num2 = Number(window.prompt("Enter the second number"));
-//     window.document.write(num1 + " is " + Math.round(num1/num2*100) + "% of " + num2 + ".");
-// }
-// percentOf();
+function percentOf() {
+    let num1 = Number(window.prompt("Enter the first number"));
+    let num2 = Number(window.prompt("Enter the second number"));
+    window.document.write('STEP 3'+ '\n')
+    window.document.write(num1 + " is " + Math.round(num1/num2*100) + "% of " + num2 + "." + '\n');
+}
+percentOf();
 
 //STEP 3 - v.2
 // function percentOf(num1, num2)
@@ -39,13 +42,14 @@
 // }
 // percentOf(2, 10);   
 
-//STEP 4 - v.1
-// function findModulus() {
-//     let num1 = Number(window.prompt("Enter the first number"));
-//     let num2 = Number(window.prompt("Enter the second number"));
-//     window.document.write(num2 % num1 + " is the modulus of " + num1 + " and " + num2 + ".");
-// }
-// findModulus();
+// STEP 4 - v.1
+function findModulus() {
+    let num1 = Number(window.prompt("Enter the first number"));
+    let num2 = Number(window.prompt("Enter the second number"));
+    window.document.write('STEP 4'+ '\n')
+    window.document.write(num2 % num1 + " is the modulus of " + num1 + " and " + num2 + ".");
+}
+findModulus();
 
 // //STEP 4 - v.2
 // function findModulus(num1, num2) {
@@ -54,11 +58,12 @@
 //     findModulus(4, 10);
 
 //STEP 5
-// function findSum(num1, num2, num3) {
-//     num1 = Number(window.prompt("Enter the first number"));
-//     num2 = Number(window.prompt("Enter the second number"));
-//     num3 = Number(window.prompt("Enter the third number"));
-//     let sum = num1 + num2 + num3;
-//     window.document.write("The sum of all of the arguments is " + sum + ".");
-// }
-// findSum();
+function findSum(num1, num2, num3) {
+    num1 = Number(window.prompt("Enter the first number"));
+    num2 = Number(window.prompt("Enter the second number"));
+    num3 = Number(window.prompt("Enter the third number"));
+    let sum = num1 + num2 + num3;
+    window.document.write('STEP 5'+ '\n')
+    window.document.write("The sum of all of the arguments is " + sum + ".");
+}
+findSum();
